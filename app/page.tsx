43a0a6748f1023855f650e707cc7030196360257
navneetrai2111 <navneet.r@zeksta.com>
2026-03-29 00:10:57 +0530

    ❤️

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,13 @@ import HomeClient from "@/components/home-client";
 
 const metaPreviewImage =
   // "https://i.pinimg.com/736x/d9/64/35/d96435c0442c2de1d33129993556331f.jpg";
-"https://i.pinimg.com/736x/30/12/85/301285f850a317256209321bffc63792.jpg";
+  "https://i.pinimg.com/736x/30/12/85/301285f850a317256209321bffc63792.jpg";
 export const metadata: Metadata = {
   title: "Wedding Album",
   description:
     "Open the wedding Album for Anandi and Vineet and continue to the private dashboard login.",
   openGraph: {
-    title: "Wedding Album | Anandi and Vineet Wedding",
+    title: "Vineet and Anandi ❤️| Wedding Album",
     description:
       "Open the wedding Album for Anandi and Vineet and continue to the private dashboard login.",
     images: [
