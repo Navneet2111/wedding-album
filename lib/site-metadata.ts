2569fd6cb4176e3d02c8b02fb88c2145aa7b2d59
navneetrai2111 <navneet.r@zeksta.com>
2026-03-29 18:39:20 +0530

    chnages

--- lib/site-metadata.ts
+++ lib/site-metadata.ts
@@ -1,4 +1,6 @@
-const ogImageFileId = "1RgXvOjPL_6bWR5k0CQuC8obQsUqlNPI4";
+// const ogImageFileId = "1RgXvOjPL_6bWR5k0CQuC8obQsUqlNPI4";
 
-export const metaPreviewImage = `https://drive.google.com/uc?export=view&id=${ogImageFileId}`;
+export const metaPreviewImage = "https://i.pinimg.com/736x/30/12/85/301285f850a317256209321bffc63792.jpg";
+
+// `https://drive.google.com/uc?export=view&id=${ogImageFileId}`;
 // "https://i.pinimg.com/736x/30/12/85/301285f850a317256209321bffc63792.jpg";
